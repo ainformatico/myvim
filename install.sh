5adc3e8ee6b3657285d000e4979fe48557229c00
Alejandro El Informático <aeinformatico@gmail.com>
2011-04-25 19:59:34 +0200

    CHANGE improved install script, create symlinks for custom snippets

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# var definitions
+SNIPMATEDIR=~/.vim/bundle/snipmate/snippets/
+CUSTOMSNIPPETS=~/.vim/my-snippets/
+TMPDIR=/tmp/
+LOCALVIMRCFILE=~/.vimrc
+CUSTOMVIMRCFILE=~/.vim/vimrc
+
 # install script
 # init git submodules
 # create symlinks and replace snippets
@@ -9,9 +16,26 @@ git submodule init
 git submodule update
 
 # create symlinks
-ln -s ~/.vim/vimrc ~/.vimrc
+if [ -f $LOCALVIMRCFILE ]
+then
+  mv $LOCALVIMRCFILE $TMPDIR
+  echo "$LOCALVIMRCFILE moved to $TMPDIR"
+fi
+ln -s $CUSTOMVIMRCFILE $LOCALVIMRCFILE
+echo "Created symlink to $LOCALVIMRCFILE"
 
 # replace snippets
-cp -a my-snippets/* ~/.vim/bundle/snipmate/snippets/
+for file in `ls $CUSTOMSNIPPETS`
+do
+ if [ -f $SNIPMATEDIR$file ]
+ then
+  # move existent snippets to $TMPDIR dir
+  mv $SNIPMATEDIR$file $TMPDIR
+  echo "Moved $SNIPMATEDIR$file to $TMPDIR$file"
+  # create symlinks to new snippets
+  ln -s $CUSTOMSNIPPETS$file $SNIPMATEDIR$file
+  echo "Created synlink to $SNIPMATEDIR$file"
+fi
+done
 
 echo "\nDone.\n"
