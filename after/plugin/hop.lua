15c7e0b535918e14a596cea561702f70451967e8
Alejandro Dev. <aeinformatico@gmail.com>
2025-08-17 10:14:30 +0200

    Set better bingdings for HopWord

--- after/plugin/hop.lua
+++ after/plugin/hop.lua
@@ -2,5 +2,8 @@ local hop = require('hop')
 
 hop.setup({})
 
-vim.keymap.set("n", ",,w", vim.cmd.HopWordAC, {})
-vim.keymap.set("n", ",,b", vim.cmd.HopWordBC, {})
+vim.keymap.set("n", "<LEADER>w", vim.cmd.HopWordMW, {})
+vim.keymap.set("n", "<LEADER>e", vim.cmd.HopChar1, {})
+vim.keymap.set("n", "<LEADER>a", vim.cmd.HopAnywhereMW, {})
+-- vim.keymap.set("n", "<LEADER><LEADER>w", vim.cmd.HopWordAC, {})
+vim.keymap.set("n", "<LEADER><LEADER>b", vim.cmd.HopWordBC, {})
