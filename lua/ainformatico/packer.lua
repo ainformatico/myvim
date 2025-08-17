c7be76ec787726ceb61b724e0584da4b350bea6c
Alejandro Dev. <aeinformatico@gmail.com>
2025-08-17 10:24:04 +0200

    Add missing modules to git

--- lua/ainformatico/packer.lua
+++ lua/ainformatico/packer.lua
@@ -62,7 +62,8 @@ return require('packer').startup(function(use)
       { 'hrsh7th/nvim-cmp' },
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'hrsh7th/cmp-buffer' },
-      { 'L3MON4D3/LuaSnip' },
+      { 'hrsh7th/cmp-nvim-lsp-signature-help' },
+      { 'hrsh7th/cmp-nvim-lua' },
     }
   }
 
