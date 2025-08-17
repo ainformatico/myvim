78e54f1aed3a3e9fffcd154a7d61da30a6ea12bc
Alejandro Dev. <aeinformatico@gmail.com>
2025-08-17 10:43:23 +0200

    Remove unnecessary config

--- after/plugin/treesitter-textobjects.lua
+++ after/plugin/treesitter-textobjects.lua
@@ -87,10 +87,3 @@ require 'nvim-treesitter.configs'.setup {
     },
   },
 }
-
-local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
-
--- Repeat movement with ; and ,
--- ensure ; goes forward and , goes backward regardless of the last direction
-vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
