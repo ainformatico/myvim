c0bed849da52cb4616cc7f11625de79fed237009
Alejandro Dev. <aeinformatico@gmail.com>
2025-09-21 15:48:56 +0200

    Use correct ruff commands

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -126,7 +126,7 @@ require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
     -- Conform will run multiple formatters sequentially
-    python = { "ruff" },
+    python = { "ruff_fix", "ruff_format" },
     -- Use a sub-list to run only the first available formatter
     javascript = { "prettierd", "prettier", stop_after_first = true },
     typescript = { "prettierd", "prettier", stop_after_first = true },
