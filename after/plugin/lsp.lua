9e837407ec416053017fcb1d8ee234cf1dc94823
Alejandro Dev. <aeinformatico@gmail.com>
2025-09-21 16:10:44 +0200

    Set borders to cmp windows

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -94,6 +94,10 @@ cmp.setup({
   }, {
     { name = 'buffer' },
   }),
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
     ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
